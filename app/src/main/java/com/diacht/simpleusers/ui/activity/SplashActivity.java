9168c4172709342d5d7c873a04a933eb19e23eed
Tetiana Diachuk <dt@dalivsoft.com>
2014-08-13 11:53:59 +0300

    add splash activity

---
+++ app/src/main/java/com/diacht/simpleusers/ui/activity/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.diacht.simpleusers.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+
+import com.diacht.simpleusers.R;
+
+/**
+ * Splash screen activity.
+ * @author Tetiana Diachuk (diacht@gmail.com)
+ */
+public class SplashActivity extends ActionBarActivity {
+    private static final long TIMEOUT_DELAY = 2000;
+    private TextView mText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mText = (TextView) findViewById(R.id.splash_text);
+        startMainActivity();
+    }
+
+    @Override
+    protected void onResume() {
+//        if(getSupportActionBar()!=null){
+//            getSupportActionBar().hide();
+//        }
+        super.onResume();
+    }
+
+    private void startMainActivity() {
+        mText.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, TIMEOUT_DELAY);
+    }
+}
